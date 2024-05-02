3698803d8c59e07479a8c11c38d4cf499f75e44d
TaufikM3 <341.mth@gmail.com>
2024-05-02 11:47:28 +0700

    https://github.com/TaufikM3/UTS40622100047.git

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Taufik from '@/components/Home/TampilanHome.vue'
+import LeftNav from '@/components/Navbar/LeftNav.vue'
+</script>
+
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar title="Application bar"></v-app-bar>
+
+    <LeftNav></LeftNav>
+    
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      <RouterView></RouterView>
+    </v-main>
+  </v-layout>
+</template>
---
+++ src/components/Navbar/LeftNav.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-navigation-drawer
+    expand-on-hover
+    rail
+    >
+    <v-list>
+        <v-list-item
+        prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+        subtitle="sandra_a88@gmailcom"
+        title="Sandra Adams"
+        ></v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-folder" title="Home" :to="'/'"></v-list-item>
+        <v-list-item prepend-icon="mdi-account-multiple" title="About" :to="'/About'"></v-list-item>
+        <v-list-item prepend-icon="mdi-star" title="Mahasiswa" :to="'/mahasiswa'"></v-list-item>
+    </v-list>
+    </v-navigation-drawer>
+</template>
